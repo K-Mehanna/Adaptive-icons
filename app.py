7d4bb96488d593837368fc1a2de215492cb6a3e6
K-Mehanna <k.mehanna25@gmail.com>
2023-05-24 19:49:24 +0100

    Added helper file with error page and requirements

--- app.py
+++ app.py
@@ -1,1 +1,46 @@
-print("Hello world!")
+# Libaries to help with the database
+import os
+
+# Libraries used 
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Functions imported from helpers.py
+from helpers import apology
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates (html webpages) are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Specifies the location to save files
+#UPLOAD_FOLDER = r"D:\Desktop\Documents\Programming Project\Test\static\images"
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Make sure API key is set
+os.environ['API_KEY'] = 'pk_94949beadebd4b6ab617125cc67c82ff'
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
+
+@app.route("/")
+def index(): 
+    return render_template("index.html")
