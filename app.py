268d8511bc4ffa41493ceda238af9813317fa541
K-Mehanna <k.mehanna25@gmail.com>
2023-05-25 16:01:19 +0100

    App works and images separated from css

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ def index():
         bottom = -1
         left = w
         right = -1
-        for i in range(2, h):
-            for j in range(2, w):
+        for i in range(3, h - 2):
+            for j in range(3, w - 2):
                 if np.sum(imageArray[i][j]) > 200:
                     if i < top:
                         top = i
@@ -106,18 +106,7 @@ def index():
         imageArray[(bottom)][right] = np.array([0, 255, 0])
         im.fromarray(imageArray).save(os.path.join(app.config['UPLOAD_FOLDER'], "edges.png"))
         rect = (left, top, (right - left), (bottom - top))
-        cv2.grabCut(img,mask,rect,bgdModel,fgdModel,10,cv2.GC_INIT_WITH_RECT)
-
-        """ # newmask is the mask image I manually labelled
-        newmask = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], "edges.png"),0)
-     
-        # wherever it is marked white (sure foreground), change mask=1
-        # wherever it is marked black (sure background), change mask=0
-        # mask[newmask == 0] = 0
-        mask[newmask == 255] = 1
-     
-        cv2.grabCut(img,mask,None,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_MASK)
-        plt.imshow(img),plt.colorbar(),plt.show() """
+        cv2.grabCut(img,mask,rect,bgdModel,fgdModel,15,cv2.GC_INIT_WITH_RECT)
 
         bColour = str(request.form.get("bColour"))
         bColourArray = np.array([int(bColour[1:3], 16), int(bColour[3:5], 16), int(bColour[5:7], 16)])
@@ -149,81 +138,3 @@ def index():
         return redirect("/")
 
     return render_template("index.html")
-
-""" @app.route("/icon-submit", methods=["GET", "POST"])
-def studentsCSV():
-    # Function to check that the file extension is allowed 
-    def allowed_file(filename):
-        allowedExtensions = {'png', 'jpg', 'jpeg'}
-        return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowedExtensions
-    
-    className = session["name"]
-
-    # When the form is submitted
-    if request.method == "POST":
-        # Inserts the name of the class into the database
-        nameID = db.execute("INSERT INTO classNames (name) VALUES (:name)", name = className)
-        timesList = session["timesList"]
-        student_numbers = session["student_numbers"] 
-        teacherID = session["user_id"] 
-
-        # Inserts each specific class instance into the database
-        for timeID in timesList:
-            db.execute(\"""INSERT INTO classes (teacherID, nameID, numberOfStudents, 
-            timeID) VALUES (:ID, :nameID, :number, :timeID)
-            \""", ID = teacherID, nameID = nameID, number = student_numbers, timeID = timeID)
-
-        i=0
-        # Variable containing a list of the names of the images
-        images = request.files.getlist('images')
-        # Variable containing the CSV file
-        names = request.files["names"]
-        if names.filename == '':
-            return apology("must include CSV")
-        # CSV file is saved into the correct directory
-        names.save(os.path.join(os.getcwd(), names.filename))
-
-        # Iterates through each line in the CSV
-        with open(names.filename, 'r') as namesfile:
-            reader = csv.reader(namesfile)
-            for row in reader:
-                # Extracts student's first and last names
-                forename = row[0].title()
-                surname = row[1].title()
-                combinedName = ' '.join(row)
-                image = images[i]
-                name = image.filename
-                if name == '':
-                    return apology("Please upload photos")
-                extension = os.path.splitext(name)[1]
-                combinedFileName = combinedName.title() + extension
-
-                # Saves image as student_name.extension
-                if image and allowed_file(name):
-                    image.save(os.path.join(app.config['UPLOAD_FOLDER'], combinedFileName))
-
-                # Tests if the student is already in the database
-                duplicateTest = db.execute(\"""SELECT studentID FROM students WHERE forename
-                 = :forename AND surname = :surname\""", forename = forename, surname = surname)
-                if len(duplicateTest) == 0:
-                    studentID = db.execute(\"""INSERT INTO students (forename, surname) VALUES 
-                    (:forename, :surname)\""", forename = forename, surname = surname)
-                else:
-                    studentID = duplicateTest[0]["studentID"]
-
-                nameDict = db.execute("SELECT nameID FROM classNames WHERE name = :name", name = className)
-                nameID = nameDict[0]["nameID"]
-
-                classIDs = db.execute(\"""SELECT classID FROM classes LEFT JOIN classNames on classes.nameID = 
-                classNames.nameID WHERE teacherID = :ID AND name = :name\""", ID = session["user_id"], name = className)
-                for item in classIDs:
-                    classID = item["classID"]
-                    # Adds student to classToStudent table which maps a student to a class
-                    insert = db.execute(\"""INSERT INTO classToStudent (classID, studentID)
-                     VALUES (:classID, :studentID)\""", classID = classID, studentID = studentID)
-                i += 1
-
-        flash("Class added!")
-        return redirect("/")
-    else:
-        return render_template("students-csv.html") """
